a8440c2f39977c8b74c945365ec466abe1c31ed0
Philip Muškovac <yofel@gmx.net>
2015-08-04 23:08:04 +0200

    fix missing stderr in logs

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -22,20 +22,20 @@ date=$(date -u +%Y%m%d)
 for arch in $architectures; do
     echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== STARTING IMAGE BUILD FOR $arch" | tee -a buildlog.management.txt
 
-    chroot ./tanglu-$arch/ /build_arch.sh prebuild | tee -a buildlog.${arch}.prepare.txt
-    chroot ./tanglu-$arch/ /build_arch.sh wipe_cache | tee -a buildlog.${arch}.prepare.txt
-    chroot ./tanglu-$arch/ /build_arch.sh expire_cache_prebuild | tee -a buildlog.${arch}.prepare.txt
-    chroot ./tanglu-$arch/ /build_arch.sh bootstrap | tee -a buildlog.${arch}.bootstrap.txt
+    chroot ./tanglu-$arch/ /build_arch.sh prebuild 2>&1 | tee -a buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ /build_arch.sh wipe_cache 2>&1 | tee -a buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ /build_arch.sh expire_cache_prebuild 2>&1 | tee -a buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ /build_arch.sh bootstrap 2>&1 | tee -a buildlog.${arch}.bootstrap.txt
     for flavor in $flavors; do
-        chroot ./tanglu-$arch/ /build_arch.sh runbuild $flavor | tee -a buildlog.${arch}.${flavor}.txt
+        chroot ./tanglu-$arch/ /build_arch.sh runbuild $flavor 2>&1 | tee -a buildlog.${arch}.${flavor}.txt
     done
 
-    chroot ./tanglu-$arch/ /build_arch.sh wipe_cache | tee -a buildlog.${arch}.finish.txt
-    chroot ./tanglu-$arch/ /build_arch.sh expire_cache_postbuild | tee -a buildlog.${arch}.finish.txt
-    chroot ./tanglu-$arch/ /build_arch.sh postbuild | tee -a buildlog.${arch}.finish.txt
+    chroot ./tanglu-$arch/ /build_arch.sh wipe_cache 2>&1 | tee -a buildlog.${arch}.finish.txt
+    chroot ./tanglu-$arch/ /build_arch.sh expire_cache_postbuild 2>&1 | tee -a buildlog.${arch}.finish.txt
+    chroot ./tanglu-$arch/ /build_arch.sh postbuild 2>&1 | tee -a buildlog.${arch}.finish.txt
 
-    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== DONE BUILDING IMAGES FOR $arch" | tee -a buildlog.management.txt
-    mv -v tanglu-$arch/tmp/cdimage/tanglu-* cdimage/$date/ | tee -a buildlog.management.txt
+    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== DONE BUILDING IMAGES FOR $arch" 2>&1 | tee -a buildlog.management.txt
+    mv -v tanglu-$arch/tmp/cdimage/tanglu-* cdimage/$date/ 2>&1 | tee -a buildlog.management.txt
 done
 
 
@@ -43,7 +43,7 @@ cd cdimage/$date
 
 echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== GENERATING CHECKSUMS" | tee -a buildlog.management.txt
 
-md5sum *.iso | tee MD5SUM | tee -a buildlog.management.txt
-sha256sum *.iso | tee SHA256SUM | tee -a buildlog.management.txt
+md5sum *.iso | tee MD5SUM 2>&1 | tee -a buildlog.management.txt
+sha256sum *.iso | tee SHA256SUM 2>&1 | tee -a buildlog.management.txt
 
 echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== IMAGE BUILD FINISHED" | tee -a buildlog.management.txt
