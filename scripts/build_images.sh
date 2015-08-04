87a46818cd4273d3668d9f19b195a686124c4ab0
Philip Muškovac <yofel@gmx.net>
2015-08-04 17:07:20 +0200

    implement split buildlogs in the image generation

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -19,32 +19,31 @@ done
 date=$(date -u +%Y%m%d)
 [ -e cdimage/$date ] || mkdir -p cdimage/$date
 
-
 for arch in $architectures; do
-    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== STARTING IMAGE BUILD FOR $arch"
+    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== STARTING IMAGE BUILD FOR $arch" | tee buildlog.management.txt
 
-    chroot ./tanglu-$arch/ build_arch.sh prebuild
-    chroot ./tanglu-$arch/ build_arch.sh wipe_cache
-    chroot ./tanglu-$arch/ build_arch.sh expire_cache_prebuild
-    chroot ./tanglu-$arch/ build_arch.sh bootstrap
+    chroot ./tanglu-$arch/ build_arch.sh prebuild | tee buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ build_arch.sh wipe_cache | tee buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ build_arch.sh expire_cache_prebuild | tee buildlog.${arch}.prepare.txt
+    chroot ./tanglu-$arch/ build_arch.sh bootstrap | tee buildlog.${arch}.bootstrap.txt
     for flavor in $flavors; do
-        chroot ./tanglu-$arch/ /build_arch.sh runbuild $flavor
+        chroot ./tanglu-$arch/ /build_arch.sh runbuild $flavor | tee buildlog.${arch}.${flavor}.txt
     done
 
-    chroot ./tanglu-$arch/ build_arch.sh wipe_cache
-    chroot ./tanglu-$arch/ build_arch.sh expire_cache_postbuild
-    chroot ./tanglu-$arch/ build_arch.sh postbuild
+    chroot ./tanglu-$arch/ build_arch.sh wipe_cache | tee buildlog.${arch}.finish.txt
+    chroot ./tanglu-$arch/ build_arch.sh expire_cache_postbuild | tee buildlog.${arch}.finish.txt
+    chroot ./tanglu-$arch/ build_arch.sh postbuild | tee buildlog.${arch}.finish.txt
 
-    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== DONE BUILDING IMAGES FOR $arch"
-    mv tanglu-$arch/tmp/cdimage/tanglu-* cdimage/$date/
+    echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== DONE BUILDING IMAGES FOR $arch" | tee buildlog.management.txt
+    mv -v tanglu-$arch/tmp/cdimage/tanglu-* cdimage/$date/ | tee buildlog.management.txt
 done
 
 
 cd cdimage/$date
 
-echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== GENERATING CHECKSUMS"
+echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== GENERATING CHECKSUMS" | tee buildlog.management.txt
 
-md5sum *.iso | tee MD5SUM
-sha256sum *.iso | tee SHA256SUM
+md5sum *.iso | tee MD5SUM | tee buildlog.management.txt
+sha256sum *.iso | tee SHA256SUM | tee buildlog.management.txt
 
-echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== IMAGE BUILD FINISHED"
+echo "[$(date -u +%Y-%m-%d\ %H:%M:%S)] ===== IMAGE BUILD FINISHED" | tee buildlog.management.txt
