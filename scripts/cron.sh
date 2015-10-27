3475fb06eba4a8ec675cba579f4a52fe1f6ad86d
Philip Muškovac <yofel@gmx.net>
2015-10-27 14:28:42 +0100

    sync cron script with the one that's deployed on the image builder

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -16,20 +16,33 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 cd $workdir
 
-./build_images.sh > buildlog 2>&1
+./build_images.sh
 
 date=$(date -u +%Y%m%d)
+version=$date;
 
-gzip -9 buildlog
-mv buildlog.gz $buildlogdir/daily-live.${date}.txt.gz
-# chown cdimage:cdimage cdimage/$date
-mv cdimage/$date $targetdir
+if [ -e $targetdir/$date ]; then
+    suffix=1
+
+    while [ -e $targetdir/${date}.$suffix ]; do
+        $suffix = $((suffix + 1));
+    done
+
+    version=${date}.$suffix
+fi
+
+cd $workdir
+mkdir -p $buildlogdir/daily-live-$version
+gzip -9 buildlog.*.txt
+mv buildlog.*.txt.gz $buildlogdir/daily-live-$version/
+# chown cdimage:cdimage cdimage/$version
+mv cdimage/$version $targetdir
 cd $targetdir
 rm -f current
-ln -sf $date current
+ln -sf $version current
 
-# only keep the last 3 images
-if [ `ls | wc -l` -gt 4 ]; then
+# only keep the last 10 images
+if [ `ls | wc -l` -gt 11 ]; then
     rm -rf `ls | head -n 1`
 fi
 
