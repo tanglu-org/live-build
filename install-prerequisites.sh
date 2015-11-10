d0bffd69e88fc9da2a9c6facf148178236a3f690
Matthias Klumpp <matthias@tenstral.net>
2015-11-10 18:44:32 +0100

    Enable gfxboot

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-apt-get install -y git live-config live-boot live-build live-tools zsync squashfs-tools dctrl-tools syslinux librsvg2-bin xorriso win32-loader loadlin
+apt-get install -y git \
+	live-config \
+	live-build \
+	live-tools \
+	squashfs-tools \
+	casper \
+	zsync \
+	dctrl-tools \
+	syslinux \
+	gfxboot-theme-tanglu \
+	librsvg2-bin \
+	graphicsmagick \
+	xorriso \
+	win32-loader \
+	loadlin
